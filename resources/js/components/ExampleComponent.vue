e15e41e5713a31b2b2e9bfdd1157356cad13ba72
Hyperstorm321 <45072321+Hyperstorm321@users.noreply.github.com>
2020-07-11 05:01:59 -0800

    FIRST UPLOAD
    FRESH UPLOAD

---
+++ resources/js/components/ExampleComponent.vue
@@ -0,0 +1,46 @@
+<template>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">Example Component</div>
+
+                    <div class="card-body">
+                        <table>
+                            <tr v-for="people in peoples" v-bind:key="people.id">
+                                <td>{{ people.first_name}}</td>
+                                <td>{{ people.last_name}}</td>
+                                <td>{{ people.user_id}}</td>
+                                <td>{{ people.email}}</td>
+                            </tr>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        data(){
+            return{
+                peoples:[],
+            }
+        },
+        mounted() {
+            this.fetchPeoples()
+        },
+        methods:{
+            fetchPeoples(page_url){
+                page_url = page_url || '/api/useraccounts';
+                fetch(page_url)
+                .then(res => res.json())
+                .then(resJson => {
+                    this.peoples = resJson
+                })
+                .catch(err => console.log(err))
+            },
+        }
+    }
+</script>
